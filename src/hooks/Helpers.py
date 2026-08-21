977d1b945df343d2582b0d9a874d9f65e0271225
Soulweaver <soulweaver@hotmail.fi>
2026-08-21 21:19:19 +0300

    Fix content availability for Christmas levels and Lori unlock
    The default behaviour for the easy category exclusion provided by
    the manual core makes it so that if any option tied to a category
    is turned off, the relevant content also follows suit.
    
    For TSF/CC/HH toggles specially, that's not true though, as some
    pieces of content are available as long as either one or two of these
    are enabled. This patch fixes their availability.

--- src/hooks/Helpers.py
+++ src/hooks/Helpers.py
@@ -1,23 +1,65 @@
 from typing import Optional, Any
 from BaseClasses import MultiWorld
 
+def enabled_if_either_hh_or_cc(multiworld: MultiWorld, player: int, thing: dict[str, Any]) -> Optional[bool]:
+    from ..Helpers import get_option_value
+
+    enable_cc = get_option_value(multiworld, player, 'enable_cc') == True
+    enable_hh = get_option_value(multiworld, player, 'enable_hh') == True
+
+    if 'category' in thing.keys():
+        if 'Holiday Hare Content' in thing['category'] and 'Christmas Chronicles Content' in thing['category']:
+            return enable_cc or enable_hh
+
+    return None
+
+def enabled_if_either_cc_or_tsf(multiworld: MultiWorld, player: int, thing: dict[str, Any]) -> Optional[bool]:
+    from ..Helpers import get_option_value
+
+    enable_cc = get_option_value(multiworld, player, 'enable_cc') == True
+    enable_tsf = get_option_value(multiworld, player, 'enable_tsf') == True
+
+    if 'category' in thing.keys():
+        if 'The Secret Files Content' in thing['category'] and 'Christmas Chronicles Content' in thing['category']:
+            return enable_cc or enable_tsf
+
+    return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the category, False to disable it, or None to use the default behavior
 def before_is_category_enabled(multiworld: MultiWorld, player: int, category_name: str) -> Optional[bool]:
     return None
 
+
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the item, False to disable it, or None to use the default behavior
-def before_is_item_enabled(multiworld: MultiWorld, player: int, item:  dict[str, Any]) -> Optional[bool]:
+def before_is_item_enabled(multiworld: MultiWorld, player: int, item: dict[str, Any]) -> Optional[bool]:
+    lori_check = enabled_if_either_cc_or_tsf(multiworld, player, item)
+    xmas_levels_check = enabled_if_either_hh_or_cc(multiworld, player, item)
+
+    if lori_check is not None:
+        return lori_check
+    elif xmas_levels_check is not None:
+        return xmas_levels_check
+        
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the location, False to disable it, or None to use the default behavior
-def before_is_location_enabled(multiworld: MultiWorld, player: int, location:  dict[str, Any]) -> Optional[bool]:
+def before_is_location_enabled(multiworld: MultiWorld, player: int, location: dict[str, Any]) -> Optional[bool]:
+    xmas_levels_check = enabled_if_either_hh_or_cc(multiworld, player, location)
+
+    if xmas_levels_check is not None:
+        return xmas_levels_check
+    
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the event, False to disable it, or None to use the default behavior
-def before_is_event_enabled(multiworld: MultiWorld, player: int, event:  dict[str, Any]) -> Optional[bool]:
+def before_is_event_enabled(multiworld: MultiWorld, player: int, event: dict[str, Any]) -> Optional[bool]:
+    xmas_levels_check = enabled_if_either_hh_or_cc(multiworld, player, event)
+
+    if xmas_levels_check is not None:
+        return xmas_levels_check
+    
     return None
