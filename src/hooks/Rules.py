32617547ea2360eca8107301eaecc87a96c91993
Soulweaver <soulweaver@hotmail.fi>
2026-08-29 04:12:01 +0300

    Implement bonus warp access rule system
    - A system for expressing the branches the player can take to get
      certain amounts of coins, and calculating the maximum the player
      can receive right now with their current items
    - Coin path definitions for all levels in Formerly a Prince

--- src/hooks/Rules.py
+++ src/hooks/Rules.py
@@ -5,6 +5,8 @@ from BaseClasses import MultiWorld, CollectionState
 
 import re
 from enum import StrEnum
+from typing import Literal, Self
+import logging
 
 class Levels(StrEnum):
     RABBIT_IN_TRAINING = 'Rabbit in Training'
@@ -325,8 +327,6 @@ def canDestroyWildcardBlocks(multiworld: MultiWorld, state: CollectionState, pla
 
 
 def canDestroyWeaponBlocks(multiworld: MultiWorld, state: CollectionState, player: int, weapon: str):
-    import logging
-
     if not weapon in Weapons:
         logging.error(f'canDestroyWeaponBlocks: invalid weapon {weapon}')
         return False
@@ -416,7 +416,6 @@ def CanReachRegion(state: CollectionState, player: int, location: str) -> bool:
 
 def hasContinuousLevelAccess(state: CollectionState, player: int, from_level: str, to_level: str) -> bool:
     from ..Rules import CanReachLocation
-    import logging
 
     end_index = 0
     try:
@@ -450,7 +449,6 @@ def hasContinuousLevelAccess(state: CollectionState, player: int, from_level: st
 
 def hasWeaponAccess(state: CollectionState, player: int, level: str, weapon: str) -> bool:
     from ..Rules import CanReachLocation
-    import logging
 
     if not weapon in Weapons:
         logging.error(f'hasWeaponAccess: invalid weapon {weapon}')
@@ -501,4 +499,305 @@ def hasWeaponAccess(state: CollectionState, player: int, level: str, weapon: str
 
     logging.debug(f'hasWeaponAccess: cannot access any weapon {weapon} locations from {level}')
     return False
+
+
+type StartPositionCharacterName = Literal['Jazz', 'Spaz']
+
+
+class CoinPathNode:
+    region: str | None = None
+    amount: int = 0
+
+    def __init__(self, amount, region = None) -> None:
+        self.amount = amount
+        self.region = region
+        pass
+
+
+class CoinPathGroup:
+    name: str
+    character: StartPositionCharacterName | None
+    branches: list['CoinPathGroup | None']
+    sequence: list['CoinPathNode | CoinPathGroup']
+    min_mode: bool
+
+    minimum_coins_from_branches = 0
+    minimum_coins_from_sequence = 0
+    minimum_coins = 0
+
+    def __init__(self, name: str, character: StartPositionCharacterName | None = None, min_mode: bool = False) -> None:
+        logging.debug(f'')
+        logging.debug(f'CoinPathGroup: init {name}')
+
+        self.name = name
+        self.character = character
+        self.min_mode = min_mode
+        self.sequence = []
+        self.branches = []
+
+    def branch(self, subpath: 'CoinPathGroup') -> Self:
+        self.branches.append(subpath)
+        
+        if self.min_mode:
+            self.minimum_coins_from_branches = min([branch.minimum_coins for branch in self.branches if branch is not None])
+        else:
+            self.minimum_coins_from_branches = max([branch.minimum_coins for branch in self.branches if branch is not None])
+
+        self._update_minimum_coins()
+        return self
     
+    def seq(self, sequence: list['CoinPathNode | CoinPathGroup']) -> Self:
+        self.sequence = sequence
+
+        self.minimum_coins_from_sequence = 0
+        for step in sequence:
+            if isinstance(step, CoinPathGroup):
+                self.minimum_coins_from_sequence += step.minimum_coins
+            elif step.region is None:
+                self.minimum_coins_from_sequence += step.amount
+
+        self._update_minimum_coins()
+        return self
+
+    def _update_minimum_coins(self):        
+        logging.debug(f'CoinPathGroup._update_minimum_coins: {self.name} - branch {self.minimum_coins_from_branches} seq {self.minimum_coins_from_sequence}')
+        self.minimum_coins = self.minimum_coins_from_branches + self.minimum_coins_from_sequence
+
+CN = CoinPathNode
+CG = CoinPathGroup
+
+COIN_ACCESS_BY_LEVEL_LOOKUP: dict[Levels, CoinPathGroup] = {
+    Levels.RABBIT_IN_TRAINING: CG(Levels.RABBIT_IN_TRAINING).seq([
+        # M3    silver: (165, 49)
+        CN(1)
+    ]),
+    Levels.DUNGEON_DILEMMA: CG(Levels.DUNGEON_DILEMMA).seq([
+        # M1    silver: (31, 15) (33, 15) (72, 2) (72, 3)
+        CN(4),
+        # M2    silver: (36, 18) (36, 19) (37, 18) (37, 19)
+        CN(4), 
+        # M4    gold:   (156, 38) (156, 39)       
+        CN(10),
+        # A4    silver: (176, 41) (177, 41) (178, 41) (179, 41) (180, 41) (186, 38) (186, 37) (186, 36)
+        CN(8, 'Gem Chute Next to Bonus Warp')
+    ]),
+    Levels.KNIGHT_CAP: CG(Levels.KNIGHT_CAP).seq([
+        CG('Start position branch', min_mode=True).branch(
+            CG('Jazz branch', 'Jazz').seq([
+                # J1     gold:   (112, 8) (112, 9) (189, 9)
+                CN(15),
+                # A2     gold:   (163, 6)
+                CN(5, 'Jazz Only Gold Coin Secret'),
+                # J3     gold:   (20, 23) (56, 31) (56, 32)
+                CN(15)
+            ])
+        ).branch(
+            CG('Spaz branch', 'Spaz').seq([
+                # A4     silver: (10, 44) (11, 44) (10, 45) (11, 45)
+                CN(4, 'Spaz Only Four Silver Coins Behind Trigger Scenery Secret'),
+                # S1     silver: (67, 54) (68, 54) (67, 55) (68, 55)
+                CN(4),
+                # A5     silver: (7, 61) (7, 62) (8, 61) (8, 62)
+                CN(4, 'Spaz Only Four Silver Coins in Secluded Room Secret'),
+                # A6     gold:   (77, 62) (78, 62)
+                CN(10, 'Spaz Only Two Gold Coins Secret'),
+                # A8     gold:   (77, 62) (78, 62)
+                CN(5, 'Spaz Only Gold Coin Secret'),
+            ])
+        ),
+        CG('Shared path').seq([
+            # M2     silver: (139, 34) (140, 34) (139, 35) (140, 35)
+            CN(4),
+            # A9     gold:   (139, 45)
+            CN(5)
+        ])
+    ]),
+    Levels.TOSSED_SALAD: CG(Levels.TOSSED_SALAD).seq([
+        CG('Start position branch', min_mode=True).branch(
+            CG('Jazz branch', 'Jazz').seq([
+                # A2    silver: (152, 18) (153, 17) (153, 18) (153, 19) (154, 18)
+                CN(5, 'Five Silver Coins Above Trigger Scenery Blocks Secret')
+            ])
+        ).branch(
+            CG('Spaz branch', 'Spaz').seq([
+                CG('Spaz branch directions').branch(
+                    CG('Into Jazz branch').seq([
+                        # A2     silver: (152, 18) (153, 17) (153, 18) (153, 19) (154, 18)
+                        CN(5, 'Five Silver Coins Above Trigger Scenery Blocks Secret')
+                    ])
+                ).branch(
+                    CG('To the end of Spaz branch').seq([
+                        # S1     silver: (182, 2) (183, 2) (182, 3) (183, 3) (202, 7) (203, 7) (204, 7) (202, 8) (203, 8) (204, 8)
+                        CN(10)
+                    ])
+                )
+            ])
+        ),
+        CG('Shared path').seq([
+            # A3     gold:   (222, 36)
+            CN(5, 'East Gold Coin Area'),
+            # M3     silver: (118, 61) (119, 61) (121, 61) (122, 61) (118, 62) (119, 62) (121, 62) (122, 62)
+            CN(8),
+            # A4     gold:   (168, 30)
+            CN(5, 'Under Jazz Section Bridge'),
+            # A5     silver: (104, 41) (104, 42)
+            CN(2, 'Two Silver Coins After Second Save Point Secret'),
+        ])
+    ]),
+    Levels.CARROT_JUICE: CG(Levels.CARROT_JUICE).seq([
+        CG('Branch at start of level').branch(
+            CG('Left path (M1)').seq([
+                # A4     gold:   (112, 57)
+                CN(5)
+            ])
+        ).branch(
+            CG('Right path (M6)').seq([])
+        ),
+        # A5     silver: (49, 3) (50, 2) (50, 3) (50, 4) (51, 3)
+        CN(5, 'Five Silver Coins Behind Blocks Secret'),
+        # A6     silver: (88, 1) (89, 1) (88, 2) (89, 2) (88, 3) (89, 3)
+        CN(6, 'Six Silver Coins Above Bubble Shield Secret'),
+        # A8     gold:   (134, 13)
+        CN(5, 'Gold Coin And Bouncer Ammo Warp Secret'),
+        # A10    gold:   (175, 4)
+        CN(5, 'Gold Coin Warp Secret')
+    ]),
+    Levels.WEIRDER_SCIENCE: CG(Levels.WEIRDER_SCIENCE).seq([
+        CG('Start position branch', min_mode=True).branch(
+            CG('Jazz branch', 'Jazz').seq([
+                # J0     silver: (2, 10)
+                CN(1),
+                # A1     silver: (43, 10) (44, 9) (44, 10) (44, 11) (45, 10)
+                CN(5, 'Jazz Super Gem and Five Silver Coins Above Trigger Scenery Secret'),
+                # A2     gold:   (73, 9)
+                CN(5, 'Jazz Gold Coin Secret')
+            ])
+        ).branch(
+            CG('Spaz branch', 'Spaz').seq([
+                # A3     gold:   (1, 37)
+                CN(5, 'Spaz Gold Coin Below Start Secret'),
+                # A4     silver: (29, 30) (30, 30) (29, 31) (30, 31) (29, 32) (30, 32)
+                CN(6, 'Spaz Silver Coins Warp Secret'),
+                # A5     silver: (37, 42)
+                CN(1, 'Spaz Silver Coins Behind Blocks Secret')
+            ])
+        ),
+        CG('Shared path').seq([
+            # A7     gold:   (122, 32)
+            CN(5, 'Gold Coin and Super Gem Above Frozen Spring Secret'),
+            # A9     gold:   (139, 55)
+            CN(5, 'Gold Coin Among Breakable Blocks Secret'),
+            # A10    gold:   (168, 43) (169, 43)
+            CN(10, 'Two Gold Coins Above Trigger Scenery Secret'),
+            # A11    silver: (161, 21) (162, 21) (161, 22) (162, 22)
+            CN(4, 'Silver Coins Behind Buttstomp Blocks Secret'),
+            # M5     gold:   (183, 11) (184, 11)
+            CN(10),
+            # M8     silver: (225, 3) (225, 4) (225, 5) (225, 6) (225, 7)
+            CN(5),
+            # M9     silver: (245, 18) (245, 19)
+            CN(2)
+        ])
+    ]),
+    Levels.LOOSE_SCREWS: CG(Levels.LOOSE_SCREWS).seq([
+        # M0     gold:   (45, 40)
+        #        silver: (0, 53) (1, 53) (0, 54) (1, 54) (22, 39) (23, 39)
+        CN(11),
+        # A2     gold:   (105, 43)
+        CN(5, 'Gold Coin Behind Vine and Buttstomp Block Secret'),
+        # A3     silver: (13, 16) (14, 16) (15, 16)
+        CN(3, 'Silver Coins Behind Destructible Wall and Springs Secret'),
+        # A5     silver: (112, 20) (113, 20) (114, 20) (112, 21) (113, 21) (114, 21)
+        CN(6, 'Airboard Backtrack Silver Coins Behind Blocks Secret'),
+        # M6     gold:   (149, 1)
+        CN(5),
+        # A6     gold:   (191, 23) (191, 24)
+        CN(10, 'Gold Coins Above Second Save Point Secret')
+    ]),
+    Levels.VICTORIAN_SECRET: CG(Levels.VICTORIAN_SECRET).seq([]),
+    Levels.COLONIAL_CHAOS: CG(Levels.COLONIAL_CHAOS).seq([]),
+    Levels.PURPLE_HAZE_MAZE: CG(Levels.PURPLE_HAZE_MAZE).seq([]),
+    Levels.FUNKY_GROOVEATHON: CG(Levels.FUNKY_GROOVEATHON).seq([]),
+    Levels.BEACH_BUNNY_BINGO: CG(Levels.BEACH_BUNNY_BINGO).seq([]),
+    Levels.MARINATED_RABBIT: CG(Levels.MARINATED_RABBIT).seq([]),
+    Levels.A_DIAMONDUS_FOREVER: CG(Levels.A_DIAMONDUS_FOREVER).seq([]),
+    Levels.FOURTEEN_CARROT: CG(Levels.FOURTEEN_CARROT).seq([]),
+    Levels.ELECTRIC_BOOGALOO: CG(Levels.ELECTRIC_BOOGALOO).seq([]),
+    Levels.VOLTAGE_VILLAGE: CG(Levels.VOLTAGE_VILLAGE).seq([]),
+    Levels.MEDIEVAL_KINEVAL: CG(Levels.MEDIEVAL_KINEVAL).seq([]),
+    Levels.HARE_SCARE: CG(Levels.HARE_SCARE).seq([]),
+    Levels.GARGOYLES_LAIR: CG(Levels.GARGOYLES_LAIR).seq([]),
+    Levels.THRILLER_GORILLA: CG(Levels.THRILLER_GORILLA).seq([]),
+    Levels.JUNGLE_JUMP: CG(Levels.JUNGLE_JUMP).seq([]),
+    Levels.A_COLD_DAY_IN_HECK: CG(Levels.A_COLD_DAY_IN_HECK).seq([]),
+    Levels.RABBIT_ROAST: CG(Levels.RABBIT_ROAST).seq([]),
+    Levels.BURNIN_BISCUITS: CG(Levels.BURNIN_BISCUITS).seq([]),
+    Levels.BAD_PITT: CG(Levels.BAD_PITT).seq([]),
+}
+
+def canCollectEnoughCoins(state: CollectionState, player: int, level: str, cost: int) -> bool:
+    coin_path: CoinPathGroup
+    try:
+        coin_path = COIN_ACCESS_BY_LEVEL_LOOKUP[Levels(level)]
+    except ValueError:
+        logging.error(f'canCollectEnoughCoins: no coin data found for level {level}')
+        return False
+
+    # short circuit if the case is trivial
+    if coin_path.minimum_coins >= cost:
+        logging.debug(f'canCollectEnoughCoins: trivial case (min: {coin_path.minimum_coins}, cost: {cost}, level: {level})')
+        return True
+
+    def recurse_coin_path_group(group: CoinPathGroup) -> int:
+        logging.debug(f'canCollectEnoughCoins: recurse group ({group.name} branches: {len(group.branches)} sequence: {len(group.sequence)})')
+        coins_collected = 0
+
+        for branch in group.branches:
+            if branch is None:
+                continue
+
+            if branch.character is not None:
+                available = False
+                if branch.character == 'Spaz':
+                    available = available or state.has('Spaz Unlock', player)
+                elif branch.character == 'Jazz':
+                    available = available or state.has('Jazz Unlock', player) or state.has('Lori Unlock', player)
+
+                if not available:
+                    logging.debug(f'canCollectEnoughCoins: {branch.name} inaccessible, requires locked character {branch.character}')
+                    continue
+
+            logging.debug(f'canCollectEnoughCoins: recurse into branch {branch.name}')
+            branch_coins_collected = recurse_coin_path_group(branch)
+            logging.debug(f'canCollectEnoughCoins: update group coins collected from {coins_collected} to {max(coins_collected, branch_coins_collected)}')
+            coins_collected = max(coins_collected, branch_coins_collected)
+
+            if coins_collected >= cost:
+                logging.debug(f'canCollectEnoughCoins: target reached, exiting recursion branch')
+                return coins_collected
+
+        for step in group.sequence:
+            logging.debug(f'canCollectEnoughCoins: next sequence step')
+            if isinstance(step, CoinPathGroup):
+                logging.debug(f'canCollectEnoughCoins: recurse into step {step.name}')
+                step_coins_collected = recurse_coin_path_group(step)
+                logging.debug(f'canCollectEnoughCoins: update group coins collected from {coins_collected} to {coins_collected + step_coins_collected}')
+                coins_collected += step_coins_collected
+            else:
+                if step.region is None or CanReachRegion(state, player, f'{level} - {step.region}'):
+                    logging.debug(f'canCollectEnoughCoins: update group coins collected from {coins_collected} to {coins_collected + step.amount} (region {step.region or 'none'})')
+                    coins_collected += step.amount
+                else:
+                    logging.debug(f'canCollectEnoughCoins: skip step (region {step.region} not reachable)')
+
+            if coins_collected >= cost:
+                logging.debug(f'canCollectEnoughCoins: target reached, exiting recursion branch')
+                return coins_collected
+
+        return coins_collected
+
+    logging.debug(f'canCollectEnoughCoins: starting main recurse (min: {coin_path.minimum_coins}, cost: {cost}, level: {level})')
+    coin_result = recurse_coin_path_group(coin_path)
+    logging.debug(f'canCollectEnoughCoins: final result (collected: {coin_result}, cost: {cost}, level: {level})')
+
+    return coin_result >= cost
